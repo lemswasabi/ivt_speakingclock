22a2fe9006bbf0db14cc9f4fc8569ae61a1421dc
Tatsunari Matsushima <tatsunari1020@gmail.com>
2021-10-21 11:36:34 +0200

    added time fuctions

--- ivt/speaking_clock.py
+++ ivt/speaking_clock.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3
+from datetime import datetime
+import pytz
 
 class SpeakingClock():
 
     def get_current_time(self):
-        raise NotImplementedError
+        tz_Amsterdam = pytz.timezone("Europe/Amsterdam")
+        dt_now = datetime.now(tz_Amsterdam)
+        hour = dt_now.hour
+        minute = dt_now.minute
+        return hour, minute
 
     def check_speaking_time(self):
-        raise NotImplementedError
+        current_time = get_current_time()
+        if 22 < current_time[0] or 8 > current_time[0]:
+            return "Block audio"
+        elif current_time[1] == 30 or current_time[1] == 0:
+            return "Play Aduio"
+        else:
+            return "Nothing"
 
     def get_audio(self):
         raise NotImplementedError
