8eb109884e6f250fbd42033c6c41e40899f6363e
lemswasabi <leminhnguyen@outlook.com>
2021-10-21 11:19:24 +0200

    add init structure

---
+++ ivt/speaking_clock.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+class SpeakingClock():
+
+    def get_current_time():
+        raise NotImplementedError
+
+    def check_speaking_time():
+        raise NotImplementedError
+
+    def get_audio():
+        raise NotImplementedError
+
+    def display():
+        raise NotImplementedError
+
+    def main_loop():
+        raise NotImplementedError
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
